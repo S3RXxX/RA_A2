d2c73a899e8b685b5150900ee7dd68fa5abec323
Iván Sánchez Gala <ivan.sanchez.gala@estudiantat.upc.edu>
2025-11-15 19:09:07 +0100

    arreglo

--- exp6metricsplots.py
+++ exp6metricsplots.py
@@ -24,25 +24,23 @@ if __name__=="__main__":
         for beta_value in sorted(df_m["beta"].unique()):
             df_beta = df_m[df_m["beta"] == beta_value]
 
-            for d_value in sorted(df_beta["d"].unique()):
-                df_d = df_beta[df_beta["d"] == d_value]
-                plt.plot(
-                    df_d["n"],
-                    df_d["gap"],
-                    label=f"d = {d_value}"
-                )
+            plt.plot(
+                df_beta["n"],
+                df_beta["gap"],
+                label=f"beta = {beta_value}"
+            )
 
-            plt.axvline(
-            x=m_value,
-            linestyle="--",
-            linewidth=1.5,
-            label="light-load"
+        plt.axvline(
+        x=m_value,
+        linestyle="--",
+        linewidth=1.5,
+        label="light-load"
         )
 
-            plt.xlabel("n")
-            plt.ylabel("std gap")
-            plt.title(f"m = {m_value} k = {beta_value+1}")
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig(f"./exp6/metrics/m{m_value}_beta{beta_value}.png")
-            plt.close()
+        plt.xlabel("n")
+        plt.ylabel("std gap")
+        plt.title(f"m = {m_value} k = {beta_value+1}")
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(f"./exp6/metrics/m{m_value}_beta{beta_value}.png")
+        plt.close()
