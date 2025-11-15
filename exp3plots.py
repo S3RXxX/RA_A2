493db0655e7967d7879e09be3ee2278c9ced9cd8
Iván Sánchez Gala <ivan.sanchez.gala@estudiantat.upc.edu>
2025-11-15 20:04:29 +0100

    3 bien :D

--- exp3plots.py
+++ exp3plots.py
@@ -9,63 +9,30 @@ if __name__=="__main__":
 
     group_cols = [c for c in df.columns if c not in ['seed', 'gap']]
     df_group = df.groupby(group_cols, as_index=False)['gap'].mean()
-    for beta_value in sorted(df_group["beta"].unique()):
-        # folder_path = f"./exp3/beta_value{beta_value}"
-        # if not os.path.exists(folder_path):
-        #     os.makedirs(folder_path)
-        df_beta = df_group[df_group["beta"] == beta_value]
-        batch_values = sorted(df_beta["b_batch"].unique())
-
-        # Dividimos los batch en grupos de 10
-        for i in range(0, len(batch_values), 10):
-            batch_subset = batch_values[i:i+10]
-
-            plt.figure()
-            for batch in batch_subset:
-                df_batch = df_beta[df_beta["b_batch"] == batch]
-                plt.plot(
-                    df_batch["n"],
-                    df_batch["gap"],
-                    label=f"batch_value = {batch}"
-                )
-
-            plt.axvline(
-                x=100,
-                linestyle="--",
-                linewidth=1.5,
-                label="light-load"
+    for batch_value in sorted(df_group["b_batch"].unique()):
+        df_batch = df_group[df_group["b_batch"] == batch_value]
+        plt.figure()
+        for beta_value in sorted(df_batch["beta"].unique()):
+            df_beta = df_batch[df_batch["beta"] == beta_value]
+            plt.plot(
+                df_beta["n"],
+                df_beta["gap"],
+                label=f"beta = {beta_value}"
             )
 
-            plt.xlabel("n")
-            plt.ylabel("gap")
-            plt.title(f"m = {100} for beta = {beta_value}, batches {i+1}-{i+len(batch_subset)}")
-            plt.legend()
-            plt.tight_layout()
+        plt.axvline(
+            x=100,
+            linestyle="--",
+            linewidth=1.5,
+            label="light-load"
+        )
 
-            # Guardamos el gráfico con índice del grupo de batches
-            plt.savefig(f"./exp3/m100_beta{beta_value}_batches{i}-{i+len(batch_subset)}.png")
-            plt.show()
+        plt.xlabel("n")
+        plt.ylabel("gap")
+        plt.title(f"m = {100} for batch_size = {batch_value}")
+        plt.legend()
+        plt.tight_layout()
 
-        # for batch in sorted(df_beta["b_batch"].unique()):
-        #     df_batch = df_beta[df_beta["b_batch"] == batch]
-
-        #     plt.plot(
-        #         df_batch["n"],
-        #         df_batch["gap"],
-        #         label=f"batch_value = {batch}"
-        #     )
-
-        # plt.axvline(
-        # x=100,
-        # linestyle="--",
-        # linewidth=1.5,
-        # label="light-load"
-        # )
-
-        # plt.xlabel("n")
-        # plt.ylabel("gap")
-        # plt.title(f"m = {100} for beta = {beta_value}")
-        # plt.legend()
-        # plt.tight_layout()
-        # plt.savefig(f"./exp3/m{100}_{beta_value}.png")
-        # plt.show()
+        # Guardamos el gráfico con índice del grupo de batches
+        plt.savefig(f"./exp3/m100_batchSize{batch_value}.png")
+        plt.close()
