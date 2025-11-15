54c4cc3c8af2b8d702f9f95874f09a47606e9438
Ivisayanel <ivansanchezgala@gmail.com>
2025-11-15 18:34:23 +0100

    exp6

---
+++ exp6.py
@@ -0,0 +1,50 @@
+from multiprocessing import Pool
+from bins import Bins
+import pandas as pd
+
+def worker(args):
+    m, n, beta, d, b_batch, seed = args
+    bins = Bins(m=m, seed=seed)
+    bins.simulate_uncertainty(d=d, n=n, beta=beta, b_size=b_batch)
+    return args, bins.gap()
+
+seeds = [42, 67, 77, 69, 13, 28430, 44015, 42699, 57975, 32146]
+T = len(seeds)
+
+if __name__=="__main__":
+    # n should go from low to n/m to n=m**2
+    # 
+    tasks = []
+
+    ##########################################
+    ####### Uncertainty experiment ###########
+    ##########################################
+    # beta=0 -> k=1 
+    # beta=1 -> k=2
+
+    betas = [x/100 for x in range(0, 101, 25)]
+    ds = [2]
+    b_batches = [1]
+    ms = [100]
+    for m in ms:
+        ns = [max(m//4, 1),max(m//2, 1)] + [x for x in range(m, m*m + 1, max(m//2,1))]
+        for n in ns:
+            for beta in betas:
+                for d in ds:
+                    for b_batch in b_batches:
+                        for seed in seeds:
+                            tasks.append((m, n, beta, d, b_batch, seed))
+
+    with Pool() as p:
+        results = p.map(worker, tasks)
+
+    args_list, gaps_list = zip(*results)
+    # Convertir args en un DataFrame
+    df_args = pd.DataFrame(args_list, columns=["m", "n", "beta", "d", "b_batch", "seed"])
+
+    # Añadir los gaps
+    df_args["gap"] = gaps_list
+
+    df = df_args
+    df.to_csv("exp6.csv",index=False)
+    
